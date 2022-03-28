1ae7e544dafce946b688b129e9ee40cf6d432750
cozynye <cozynye@gmail.com>
2022-03-28 17:55:15 +0900

    Docs : 20220328 알고리즘 : 키패드 누르기

--- README.md
+++ README.md
@@ -1,7 +1,15 @@
 # 🏋️ 신수녕 자바스크립트, cs, 알고리즘 공부 레파지토리입니다!
 
-## 자바스크립트
+## 🚩 자바스크립트
 
-1. [옵셔널 체이닝](javascript/20220328.md) (updated 2022-03-28)
+1. [옵셔널 체이닝](javascript/20220328.md)
+
+<br>
+
+## 🍉 코딩 테스트
+
+1. [키패드 누르기](algorithm/codingTest1.js) / [프로그래머스](https://programmers.co.kr/learn/courses/30/lessons/67256)
+
+- 중복되는 코드가 너무 많고 조건문을 많이 썼다. 줄일 수 있으면 줄여보자
 
 <hr>
---
+++ algorithm/codingTest1.js
@@ -0,0 +1,153 @@
+
+function solution(numbers, hand) {
+    var answer = '';
+    const phoneNumbers =[
+        [1,2,3],
+        [4,5,6],
+        [7,8,9],
+        ['*',0,'#']
+    ];
+    let leftHand = '*';
+    let rightHand = '#';
+
+    numbers.forEach((num,i)=>{
+        if(num===1 || num===4 || num===7){
+            leftHand=num;
+            answer = answer + 'L';
+        }else if(num === 3 || num === 6 || num === 9){
+            rightHand=num;
+            answer = answer + 'R';
+        }else{
+            let numCoordinate=[];
+            let leftCoordinate=[];
+            let rightCoordinate=[];
+            phoneNumbers.forEach((e,leftIndex)=>{
+                e.forEach((e,rightIndex)=>{
+                    if(e ===num){
+                        numCoordinate.push(leftIndex, rightIndex)
+                    }
+                    if(e === leftHand){
+                        leftCoordinate.push(leftIndex, rightIndex)
+                    }
+                    if(e === rightHand){
+                        rightCoordinate.push(leftIndex, rightIndex)
+                    }
+                    
+                })
+            })
+            if(Math.abs(numCoordinate[0]-leftCoordinate[0])+Math.abs(numCoordinate[1]-leftCoordinate[1])>
+            Math.abs(numCoordinate[0]-rightCoordinate[0])+Math.abs(numCoordinate[1]-rightCoordinate[1])){
+                rightHand=num;
+                answer = answer + 'R';
+            }else if(Math.abs(numCoordinate[0]-leftCoordinate[0])+Math.abs(numCoordinate[1]-leftCoordinate[1])<
+            Math.abs(numCoordinate[0]-rightCoordinate[0])+Math.abs(numCoordinate[1]-rightCoordinate[1])){
+                leftHand=num;
+                answer = answer + 'L';
+            }else{
+                if(hand==='right'){
+                    rightHand=num;
+                    answer = answer + 'R';
+                }else{
+                    leftHand=num;
+                    answer = answer + 'L';
+                }
+            }
+        }
+    })
+        
+    return answer;
+}
+
+
+/*
+진짜 어거지로 푼거 같음 다른 사람 풀이 보고 다른 풀이 방법 생각해볼 수 있도록 공부해야겠음
+function solution(numbers, hand) {
+    var answer = '';
+    const phoneNumbers =[
+        [1,2,3],
+        [4,5,6],
+        [7,8,9],
+        ['*',0,'#']
+    ];
+    let leftHand = '*';
+    let rightHand = '#';
+
+    numbers.forEach((num,i)=>{
+        console.log('순서', i+1,  '번째이고  숫자',num)
+        if(num===1 || num===4 || num===7){
+            leftHand=num;
+            answer = answer + 'L';
+        }else if(num === 3 || num === 6 || num === 9){
+            rightHand=num;
+            answer = answer + 'R';
+        }else{
+            let numCoordinate=[];
+            let leftCoordinate=[];
+            let rightCoordinate=[];
+            phoneNumbers.forEach((e,leftIndex)=>{
+                e.forEach((e,rightIndex)=>{
+                    console.log('반복문 내 숫자', e)
+                    if(e ===num){
+                        console.log('num', num)
+                        console.log('leftIndex', leftIndex)
+                        console.log('rightIndex', rightIndex)
+                        numCoordinate.push(leftIndex, rightIndex)
+                    }
+                    if(e === leftHand){
+                        console.log('왼손위치 찾기')
+                        console.log('leftHand', leftHand)
+                        console.log('leftIndex', leftIndex)
+                        console.log('rightIndex', rightIndex)
+                        leftCoordinate.push(leftIndex, rightIndex)
+                    }
+                    if(e === rightHand){
+                        console.log('오른손위치 찾기')
+                        console.log('rightHand', rightHand)
+                        console.log('leftIndex', leftIndex)
+                        console.log('rightIndex', rightIndex)
+                        rightCoordinate.push(leftIndex, rightIndex)
+                    }
+                    
+                })
+            })
+            console.log('numCoordinate,',numCoordinate)
+            console.log('leftCoordinate,',leftCoordinate)
+            console.log('rightCoordinate,',rightCoordinate)
+            console.log('순서가 끝났음');
+            console.log('왼손 거리',Math.abs(numCoordinate[0]-leftCoordinate[0])+Math.abs(numCoordinate[1]-leftCoordinate[1]));
+            console.log('오른손 거리',Math.abs(numCoordinate[0]-rightCoordinate[0])+Math.abs(numCoordinate[1]-rightCoordinate[1]));
+            if(Math.abs(numCoordinate[0]-leftCoordinate[0])+Math.abs(numCoordinate[1]-leftCoordinate[1])>
+            Math.abs(numCoordinate[0]-rightCoordinate[0])+Math.abs(numCoordinate[1]-rightCoordinate[1])){
+                console.log('왼손이 더 멀다');
+                rightHand=num;
+                answer = answer + 'R';
+            }else if(Math.abs(numCoordinate[0]-leftCoordinate[0])+Math.abs(numCoordinate[1]-leftCoordinate[1])<
+            Math.abs(numCoordinate[0]-rightCoordinate[0])+Math.abs(numCoordinate[1]-rightCoordinate[1])){
+                console.log('오른손이 더 멀다');
+                leftHand=num;
+                answer = answer + 'L';
+            }else{
+                if(hand==='right'){
+                    rightHand=num;
+                    answer = answer + 'R';
+                }else{
+                    leftHand=num;
+                    answer = answer + 'L';
+                }
+            }
+        }
+    })
+    console.log('answer : ', answer)
+        
+    return answer;
+}
+
+let a=[1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5];
+// 1  *  #  L
+// 3  1  #  LR
+// 4  1  3  LRL
+// 5  4  3  LRLL
+// 8  5  3  LRLLL
+// "LRLLLRLLRRL"
+console.log(solution(a,'right'))
+*/
